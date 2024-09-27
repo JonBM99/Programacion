3cbc4b92e4230ef88fce96f33432a27badc4984f
DESKTOP-IA1GOEK\daw1 <jblancom06@educantabria.es>
2024-09-27 09:24:52 +0200

    Ejercicios clase

---
+++ Prueba/src/com/decroly/daw/Main.java
@@ -0,0 +1,34 @@
+package com.decroly.daw;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!!!");
+		System.out.println("Hola Decroly DAW");
+		
+		String text1 = "¿Como te llamas?";
+		String text2 = "Paco";
+		
+		int num = 0; //vale 0 si no le das valor
+		int a = 23;
+		double numR = 33.33;
+	
+		int suma = a + num;
+		
+		boolean isEnable= false;
+		char c ='S';
+		
+		System.out.println(num);
+		System.out.println((a != num));
+
+		System.out.println(text1 + text2);
+		System.out.println("test linea");
+
+		
+		System.out.println(text1 + " " + text2 );
+		System.out.println("Edad: "+ a);
+		
+
+	}
+}
